ee4c6fd0ab8882564cac579bfd42778d9ac18fc8
CC11001100 <CC11001100@qq.com>
2023-01-10 11:32:53 +0800

    添加说明

--- examples/set_default/main.go
+++ examples/set_default/main.go
@@ -19,6 +19,10 @@ func Foo(options ...*FooOptions) {
 	// 如果没有传递参数的话，则设置一个默认的参数
 	options = variable_parameter.SetDefaultParam(options, DefaultFooOptions)
 
+	// 为什么不适用这种方式来操作呢？也许这样更简单？
+	// 在递归互相调用的时候切片中可能会被重复放入默认值，尤其是在有很多的重载之类的或者高内聚的代码中会互相调用问题尤其明显
+	//options = append(options, DefaultFooOptions)
+
 	// 后面的代码就可以直接使用options[0]来操作而不必担心越界
 	fmt.Println(options[0].Foo)
 }
